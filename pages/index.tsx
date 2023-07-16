7d27be9134b6f5a1d3f377f16a303207feed88a2
solengha <solengha@vodfactory.com>
2023-07-16 03:42:22 +0200

    Navbar creation

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,7 @@
 import useCurrentUser from "@/hooks/useCurrentUser"
 import { NextPageContext } from "next"
 import { getSession, signOut } from "next-auth/react"
+import Navbar from "@/components/Navbar"
 
 export async function getServerSideProps(context:NextPageContext) {
   const session = await getSession(context)
@@ -21,13 +22,9 @@ export async function getServerSideProps(context:NextPageContext) {
 
 export default function Page() {
 
-  const { data: user } = useCurrentUser()
-
   return (
     <>
-      <h1 className="text-5xl">Netflix Clone</h1> 
-      <p className="test-white">Logged in as : {user?.email}</p>
-      <button className="bg-red-600 p-3 text-white rounded-md w-full mt-10 hover:bg-red-700 transition-colors" onClick={() => signOut()}>Sign out</button>  
+      <Navbar />
     </>
   ) 
 }
